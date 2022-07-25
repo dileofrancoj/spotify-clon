ac6f50480f3fcf5c9b368320e65efbbdb666d8a8
Diego Gandara <diego.gandara@pedidosya.com>
2022-07-25 20:16:09 -0300

    trying to break commitlink by not passing chore, feat, fix or any other prefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  eslint: {
+    rules: {
+      "react/prop-types": 0,
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
