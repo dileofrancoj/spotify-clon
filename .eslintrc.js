e4e32ef23c9f049c5674f5516a70838f26e957c2
Diego Gandara <diego.gandara@pedidosya.com>
2022-07-25 19:41:05 -0300

    feat: change/extend eslint config with nextJS recommended settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,21 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  // extends: ['eslint:recommended', 'plugin:react/recommended'],
+  extends: ['plugin:@next/next/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
-}
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 'off',
+  },
+};
