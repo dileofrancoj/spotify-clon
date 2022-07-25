5fa6385ef277720cb1d62ab97038e8aaed23421d
Diego Gandara <diego.gandara@pedidosya.com>
2022-07-25 20:16:56 -0300

    come on, now it got to work

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,12 +1,3 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  parserPreset: 'conventional-changelog-atom',
-  formatter: '@commitlint/format',
-  /*
-   * Any rules defined here will override rules from @commitlint/config-conventional
-   */
-  /* rules: {
-    'type-enum': [2, 'always', ['foo']],
-  },
-  */
+  extends: ["@commitlint/config-conventional"],
 };
