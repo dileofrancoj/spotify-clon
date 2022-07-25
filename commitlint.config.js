38c39614dea8ba2aced4ec37d7bf2d7fcc137e82
Diego Gandara <diego.gandara@pedidosya.com>
2022-07-25 20:10:59 -0300

    chore: add more configuration to commitlint.config file

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  parserPreset: 'conventional-changelog-atom',
+  formatter: '@commitlint/format',
+  /*
+   * Any rules defined here will override rules from @commitlint/config-conventional
+   */
+  /* rules: {
+    'type-enum': [2, 'always', ['foo']],
+  },
+  */
 };
